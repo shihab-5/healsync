a4c12f15828741accec46bf35546af2fdeea36a6
shihab-5 <shihabuli364@gmail.com>
2026-06-29 18:47:42 +0600

    deploy error fixed

---
+++ src/app/dashboard/doctor/manageSchedule/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            thank you for booking an appointment. You will receive a confirmation email shortly.
+        </div>
+    );
+};
+
+export default page;
